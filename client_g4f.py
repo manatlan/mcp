099026ef56dc12e07dbd67d43fcaeeb8ab5bf299
manatlan <manatlan@gmail.com>
2025-05-30 17:54:03 +0200

    cleaner

--- client_g4f.py
+++ client_g4f.py
@@ -10,8 +10,19 @@ patch_infer_model()
 from pydantic_ai import Agent
 
 from pydantic_ai.mcp import MCPServerHTTP,MCPServerStdio
-server = MCPServerStdio("uvx",["mcp-server-time","--local-timezone=Europe/Paris"])
+servers = [
+    # https://pypi.org/project/mcp-server-time/
+    MCPServerStdio("uvx",["mcp-server-time","--local-timezone=Europe/Paris"]),
 
+    # local server, cf "./mcp-server-test"
+    MCPServerStdio("uv",[
+        "--directory",
+        "./mcp-server-test",
+        "run",
+        "mcp-server-test"
+      ]),
+]
+# servers=[]
 
 # Define the agent
 agent = Agent(
@@ -19,18 +30,23 @@ agent = Agent(
     # "g4f:deepseek-v3",
     # "g4f:gemini-1.5-flash",
     # system_prompt='Be concise, reply with one sentence.',
-    mcp_servers=[server])  
+    mcp_servers=servers)  
 
 
 # @agent.tool_plain
 # def get_current_time() -> str:
 #     return datetime.datetime.now().isoformat()
 
-
 async def main():
     async with agent.run_mcp_servers():  
-        result = await agent.run('quel heure à tokyo?')
-    print(result.output)
+        async def ask(question):
+            print(">>>", question)
+            result = await agent.run(question)
+            print("<<<", result.output)
+
+        await ask('quelle heure à montreal ?')
+        await ask('quelle heure à tokyo?')
+        await ask('ajoute une note toto, avec comme contenu l\'heure de kiev ! ')
 
 import asyncio
 if __name__ == '__main__':
